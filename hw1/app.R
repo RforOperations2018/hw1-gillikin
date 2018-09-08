b3659e7c9dfb3690b5e2823dde79e2d74ed1de76
gillikin <bgilliki@andrew.cmu.edu>
2018-09-07 21:16:56 -0400

    Trying to commit

--- hw1/app.R
+++ hw1/app.R
@@ -1,43 +1,57 @@
-
-
+# Libraries
 library(shiny)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+# Set variable for plot
+petal.l <-  as.character(unique(iris$Petal.Length))
+names(petal.l) <- petal.l
+
+
+# Define UI for application 
+ui <- navbarPage("Iris Time",
+                 # Plot navbar, which isn't particularily meaningful, but it does run
+                 tabPanel("Plot",
+                          sidebarLayout(
+                            sidebarPanel(
+                              selectInput('Petal.Length','Petal.Length',choices = petal.l)
+                              
+                            ),
+                            mainPanel(
+                            plotOutput('distPlot')  
+                            )
+                          ) 
+                 ),
+                 # Table navbar
+                 tabPanel("Table",
+                          DT::dataTableOutput("table")
+                 ),
+                 # Input navbar because I needed another input but unfortunately don't have time to develop this further
+                 tabPanel("Input",
+                          checkboxGroupInput("checkGroup", label = h3("Iris Petals"), 
+                                             choices = list("Length" = 1, "Width" = 2, "Species" = 3),
+                                             selected = 1),
+                          
+                          
+                          hr(),
+                          fluidRow(column(3, verbatimTextOutput("value")))
+                          
+                 )
 )
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  
+# Define server logic
+server <- function(input, output, session) {
+  output$distPlot <- renderPlot({
+    newdata <- subset(iris, iris$Petal.Length==input$Petal.Length)
+    ggplot(newdata, aes(x=Sepal.Width)) + geom_histogram(fill = "steelblue")
+  })
+  
+  output$table <- DT::renderDataTable({
+    DT::datatable(iris)
+  })
+  
+  output$value <- renderPrint({ input$checkGroup 
+  })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
